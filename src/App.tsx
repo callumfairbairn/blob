0e2aff5233b606bb32d61decd9ddb3f7ed0dadc6
callumfairbairn <callum.fairbairn@thoughtworks.com>
2021-05-23 16:32:57 +0100

    Use Card in App and change css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Card} from "./Card/Card";
 
 function App() {
   return (
     <div className="App">
-      Sup
+      <Card  value={4}/>
     </div>
   );
 }
