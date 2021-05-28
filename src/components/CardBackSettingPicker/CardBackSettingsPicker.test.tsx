75bd9f670fd448c338a5ddf29ad0fd860855af66
callumfairbairn <callum.fairbairn@thoughtworks.com>
2021-05-28 17:50:03 +0100

    set limit on how many diamonds user can choose

--- src/components/CardBackSettingPicker/CardBackSettingsPicker.test.tsx
+++ src/components/CardBackSettingPicker/CardBackSettingsPicker.test.tsx
@@ -1,36 +1,69 @@
-import { CardBackSettingsPicker } from './CardBackSettingsPicker'
+import { CardBackSettingsPicker, diamondLimit } from './CardBackSettingsPicker'
 import { render, screen } from '@testing-library/react'
 import { AppContextProvider } from '../../AppContext/AppContext'
 import userEvent from '@testing-library/user-event'
 
 describe('CardBackSettingsPicker', () => {
-  it('increases the number of diamonds on the cards when the up button is clicked', () => {
-    const setCardBackDiamondNumber = jest.fn()
-    const appProviderValues = { cardBackDiamondNumber: 6, setCardBackDiamondNumber }
+  describe('when increasing number of diamonds', () => {
+    it('calls setCardBackDiamondNumber with a number one greater than the current number', () => {
+      const setCardBackDiamondNumber = jest.fn()
+      const appProviderValues = { cardBackDiamondNumber: 6, setCardBackDiamondNumber }
 
-    render(
-      <AppContextProvider values={appProviderValues}>
-        <CardBackSettingsPicker />
-      </AppContextProvider>
-    )
+      render(
+        <AppContextProvider values={appProviderValues}>
+          <CardBackSettingsPicker />
+        </AppContextProvider>
+      )
 
-    userEvent.click(screen.getByTestId('moreDiamondsButton'))
+      userEvent.click(screen.getByTestId('moreDiamondsButton'))
 
-    expect(setCardBackDiamondNumber).toHaveBeenCalledWith(7)
+      expect(setCardBackDiamondNumber).toHaveBeenCalledWith(7)
+    })
+
+    it('does not increase the number when the limit is hit', () => {
+      const setCardBackDiamondNumber = jest.fn()
+      const appProviderValues = { cardBackDiamondNumber: diamondLimit, setCardBackDiamondNumber }
+
+      render(
+        <AppContextProvider values={appProviderValues}>
+          <CardBackSettingsPicker />
+        </AppContextProvider>
+      )
+
+      userEvent.click(screen.getByTestId('moreDiamondsButton'))
+
+      expect(setCardBackDiamondNumber).toHaveBeenCalledWith(diamondLimit)
+    })
   })
 
-  it('decreases the number of diamonds on the cards when the down button is clicked', () => {
-    const setCardBackDiamondNumber = jest.fn()
-    const appProviderValues = { cardBackDiamondNumber: 6, setCardBackDiamondNumber }
+  describe('when decreasing number of diamonds', () => {
+    it('calls setCardBackDiamondNumber with a number one less than the current number', () => {
+      const setCardBackDiamondNumber = jest.fn()
+      const appProviderValues = { cardBackDiamondNumber: 6, setCardBackDiamondNumber }
 
-    render(
-      <AppContextProvider values={appProviderValues}>
-        <CardBackSettingsPicker />
-      </AppContextProvider>
-    )
+      render(
+        <AppContextProvider values={appProviderValues}>
+          <CardBackSettingsPicker />
+        </AppContextProvider>
+      )
 
-    userEvent.click(screen.getByTestId('fewerDiamondsButton'))
+      userEvent.click(screen.getByTestId('fewerDiamondsButton'))
 
-    expect(setCardBackDiamondNumber).toHaveBeenCalledWith(5)
+      expect(setCardBackDiamondNumber).toHaveBeenCalledWith(5)
+    })
+    it('does not decrease the number when it is 1', () => {
+      const setCardBackDiamondNumber = jest.fn()
+      const appProviderValues = { cardBackDiamondNumber: 1, setCardBackDiamondNumber }
+
+      render(
+        <AppContextProvider values={appProviderValues}>
+          <CardBackSettingsPicker />
+        </AppContextProvider>
+      )
+
+      userEvent.click(screen.getByTestId('fewerDiamondsButton'))
+
+      expect(setCardBackDiamondNumber).toHaveBeenCalledWith(1)
+    })
   })
 })
--- src/components/CardBackSettingPicker/CardBackSettingsPicker.tsx
+++ src/components/CardBackSettingPicker/CardBackSettingsPicker.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import { AppContext } from '../../AppContext/AppContext'
 import './CardBackSettingsPicker.scss'
 
+export const diamondLimit = 35;
+
 export const CardBackSettingsPicker = () => {
   const { cardBackDiamondNumber, setCardBackDiamondNumber } = useContext(AppContext)
 
@@ -14,13 +16,21 @@ export const CardBackSettingsPicker = () => {
           <div
             className="diamondsButton"
             id="moreDiamondsButton"
-            onClick={() => setCardBackDiamondNumber(cardBackDiamondNumber + 1)}
+            onClick={
+              () => setCardBackDiamondNumber(
+                cardBackDiamondNumber === diamondLimit ? diamondLimit : cardBackDiamondNumber + 1
+              )
+            }
             data-testid="moreDiamondsButton"
           />
           <div
             className="diamondsButton"
             id="fewerDiamondsButton"
-            onClick={() => setCardBackDiamondNumber(cardBackDiamondNumber - 1)}
+            onClick={
+              () => setCardBackDiamondNumber(
+                cardBackDiamondNumber === 1 ? 1 : cardBackDiamondNumber - 1
+              )
+            }
             data-testid="fewerDiamondsButton"
           />
         </div>
